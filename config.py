b9ecfd8b6ca0531287a02dc52e3e9ca4c21d2d86
XiaoFuXin <1978147795@qq.com>
2026-04-27 19:51:59 +0800

    添加项目配置文件 config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+#配置环境
+
+#数据库配置
+DB_HOST = "localhost"          # 数据库地址
+DB_PORT = 3306                 # 端口
+DB_USER = "root"               # 用户名
+DB_PASSWORD = "123456"          # 密码
+DB_NAME = "gaokao_db"           #数据库名
+
+#爬虫目标网址
+TARGET_URL = "待定"
+
+# 其他配置
+OUTPUT_DIR = "outputs"
